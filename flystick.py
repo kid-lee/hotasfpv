4fda44ac51a9fda46207554bdf352afd9d546c16
kid-lee <kidd.leer@gmail.com>
2021-03-25 22:48:38 +0000

    clean code

--- flystick.py
+++ flystick.py
@@ -15,9 +15,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from flystick_config import (
     CHANNELS, DISPLAY, DISPLAY_BRIGHTNESS, PPM_OUTPUT_PIN)
 
-import pygame
-from pygame.locals import *
 import logging
+import pygame
 import signal
 import threading
 import time
@@ -29,10 +28,43 @@ except ImportError as e:
     logging.warn("Failed to load pigpio library, running in debug mode")
     pigpio = None
 
+try:
+    import scrollphat
+except (ImportError, IOError) as e:
+    logging.warn(e, exc_info=True)
+    logging.warn("Failed to load Scroll pHAT library, you'll be missing all the fancy graphics")
+    scrollphat = None
+
 
 _running = False
 
 _output = ()
+
+
+def render():
+    # LED check
+    scrollphat.clear_buffer()
+    for col in range(0, 13):
+        if col > 1:
+            scrollphat.set_col(col - 2, 0)
+        if col < 11:
+            scrollphat.set_col(col, 0b11111)
+        scrollphat.update()
+        time.sleep(.1)
+
+    time.sleep(.3)
+
+    while _running:
+        scrollphat.clear_buffer()
+        # ``_output`` access should be thread-safe; de-referenced just once
+        for rend, value in zip(DISPLAY, _output):
+            try:
+                rend(value, scrollphat)
+            except ValueError as e:
+                logging.warn(e, exc_info=True)
+        scrollphat.update()
+        time.sleep(.05)
+
 
 def shutdown(signum, frame):
     global _running
@@ -41,15 +73,14 @@ def shutdown(signum, frame):
 
 def main():
     global _output
+
     pygame.init()
+
     # Reading only "clicks" via events. These are used for advanced
     # mappings. Events to avoid tracking state manually. Axes are read
     # by snapshotting.
-    pygame.event.set_allowed([pygame.JOYBUTTONDOWN,pygame.JOYHATMOTION])
-    
-    # Reading only "clicks" via events. These are used for advanced
-    # mappings. Events to avoid tracking state manually. Axes are read
-    # by snapshotting.
+    pygame.event.set_allowed([pygame.JOYBUTTONDOWN,
+                              pygame.JOYHATMOTION])
 
     pi_gpio = 1 << PPM_OUTPUT_PIN
 
@@ -63,20 +94,30 @@ def main():
     else:
         pi = None
 
+    if scrollphat:
+        scrollphat.clear()
+        scrollphat.set_brightness(DISPLAY_BRIGHTNESS)
+        # fork to avoid crash in case of I2C connection issues
+        th = threading.Thread(target=render)
+        th.daemon = True
+        th.start()
 
     prev = None
 
     while _running:
-         # clicks for advanced mapping
+        # clicks for advanced mapping
         clicks, hats = [], []
-        
         for evt in pygame.event.get():
             if evt.type == pygame.JOYBUTTONDOWN:
-                    #print "JOYBUTTONDOWN: %r\n%s" % (evt, dir(evt))
+                #print "JOYBUTTONDOWN: %r\n%s" % (evt, dir(evt))
                 clicks.append(evt)
             elif evt.type == pygame.JOYHATMOTION and any(evt.value):
-                    #print "JOYHATMOTION: %r\n%s" % (evt, dir(evt))
+                #print "JOYHATMOTION: %r\n%s" % (evt, dir(evt))
                 hats.append(evt)
+
+        # tuple to enforce immutability
+        _output = tuple(max(min(ch((clicks, hats)), 1.), -1.)
+                        for ch in CHANNELS)
 
         if _output == prev:
             # do nothing
@@ -86,7 +127,7 @@ def main():
             pulses, pos = [], 0
             for value in _output:
                 # calibrated with Taranis to [-99.6..0..99.4]
-                us = int(round(1000 + 453 * value))
+                us = int(round(1500 + 500 * value))
                 pulses += [pigpio.pulse(0, pi_gpio, 300),
                            pigpio.pulse(pi_gpio, 0, us - 300)]
                 pos += us
@@ -112,8 +153,8 @@ def main():
         # very sophisticated. (At this point, at least.)
         time.sleep(.02)
 
-
-
+    if scrollphat:
+        scrollphat.clear()
     if pi:
         pi.stop()
 
